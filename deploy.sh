dc34ea76f7967ed97c7972d57d67755d90c5077c
honoka4h <honoka4h@gmail.com>
2025-12-06 17:16:32 +0900

    test commit

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd /var/www/chat-app/Backend
 git pull origin main
 npm install
 npm run build
-pm2 restart chat-app || pm2 start /dist/main.js  --name "chat-app"
+pm2 restart chat-app || pm2 start dist/main.js  --name "chat-app"
 
 # Frontend
 cd /var/www/chat-app/Frontend
