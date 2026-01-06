89c6a79482c403e9d3a0e6c2ac8fea7c01e3a918
velocibet <honoka4h@gmail.com>
2026-01-06 14:38:47 +0900

    add: google analytics

--- Frontend/app/app.vue
+++ Frontend/app/app.vue
@@ -1,3 +1,14 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1P0422E37C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1P0422E37C');
+</script>
+
+
 <template>
   <div>
     <NuxtLayout>
