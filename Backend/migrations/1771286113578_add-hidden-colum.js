8b22f45face5a3bfa64f8688dd6340606edfaf1e
velocibet <honoka4h@gmail.com>
2026-02-17 08:56:13 +0900

    Update DB schema

---
+++ Backend/migrations/1771286113578_add-hidden-colum.js
@@ -0,0 +1,17 @@
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+export const shorthands = undefined;
+
+export const up = (pgm) => {
+  const currentDir = fileURLToPath(new URL('.', import.meta.url));
+  const sqlPath = join(currentDir, '../db_migration/20260216234939508_add_hidden_colum.sql');
+  
+  const sql = readFileSync(sqlPath, 'utf-8');
+  pgm.sql(sql);
+};
+
+export const down = (pgm) => {
+  // 롤백 필요 시 작성
+};
